2a31206434332df665a8e7afa379c9c8184c1864
DungPhanHoangg05 <143725969+DungPhanHoangg05@users.noreply.github.com>
2025-05-07 12:28:23 +0700

    Update recommendations_controller.py

--- app/controllers/recommendations_controller.py
+++ app/controllers/recommendations_controller.py
@@ -1,13 +1,13 @@
 from fastapi import APIRouter, Depends
 from app.services.recommendation_service import RCMNewsSystem
-from app.databases.mongodb import get_news_collection, get_user_collection
+from app.databases.mongodb import get_article_collection, get_user_collection
 
 router = APIRouter(prefix="/news")
 
 @router.get("/{news_id}")
-async def get_news(news_id: str, news_collection=Depends(get_news_collection), user_collection=Depends(get_user_collection)):
+async def get_news(news_id: str, news_collection=Depends(get_article_collection), user_collection=Depends(get_user_collection)):
     return await RCMNewsSystem(news_collection, user_collection).get_news(news_id)
 
 @router.get("/view/{news_id}")
-async def view_news(news_: str, news_collection=Depends(get_news_collection), user_collection=Depends(get_user_collection)):
+async def view_news(news_: str, news_collection=Depends(get_article_collection), user_collection=Depends(get_user_collection)):
     return await RCMNewsSystem(news_collection, user_collection).show_news(news_id)
